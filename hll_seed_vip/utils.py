9fc75009715b9b413956982a6c6ce4c5829085f1
C. Eric Mathey <emathey@protonmail.com>
2024-01-19 13:29:29 -0700

    Fix logging

--- hll_seed_vip/utils.py
+++ hll_seed_vip/utils.py
@@ -66,7 +66,7 @@ def check_population_conditions(config: ServerConfig, gamestate: GameState):
 
     if not all_met(player_count_conditions):
         logger.debug(
-            f"{player_count_conditions[0]}={player_count_conditions[0].is_met()=} {player_count_conditions[1]}={player_count_conditions[1].is_met()=} breaking",
+            f"{player_count_conditions[0]}={player_count_conditions[0].is_met()} {player_count_conditions[1]}={player_count_conditions[1].is_met()} breaking",
         )
         return False
 
